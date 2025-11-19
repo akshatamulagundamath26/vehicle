0aef36c75745afd16392548c720b6c37df2d889f
akshatamulagundamath26 <akshatamulagundamath26@gmail.com>
2025-11-19 10:52:16 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,22 +1,21 @@
 import sys
 
-def print_vehicle_info():
-    if len(sys.argv) != 5:
-        print("Usage: python vehicle.py <car_number> <owner_name> <vehicle_type> <rc_expiry_year>")
-        sys.exit(1)
-
+if len(sys.argv) == 5:
     car_number = sys.argv[1]
     owner_name = sys.argv[2]
     vehicle_type = sys.argv[3]
     rc_expiry_year = sys.argv[4]
+    print("User inputs provided")
+else:
+    car_number = "KA01AB1234"
+    owner_name = "xyz"
+    vehicle_type = "abc"
+    rc_expiry_year = "2025"
+    print("Default inputs used")
 
-    print("Vehicle Registration Info:")
-    print("-------------------------")
-    print(f"Car Number: {car_number}")
-    print(f"Owner Name: {owner_name}")
-    print(f"Vehicle Type: {vehicle_type}")
-    print(f"RC Expiry Year: {rc_expiry_year}")
-
-if __name__ == "__main__":
-    print_vehicle_info()
-
+print("Vehicle Registration Info:")
+print("-------------------------")
+print(f"Car Number: {car_number}")
+print(f"Owner Name: {owner_name}")
+print(f"Vehicle Type: {vehicle_type}")
+print(f"RC Expiry Year: {rc_expiry_year}")
